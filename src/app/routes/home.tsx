76bbb57a056829c4b2c4c6ae0e95df572410b42d
kon-mtal <konradmstlr@gmail.com>
2025-09-30 18:12:14 +0200

    Implement backend interaction, CORS problem

--- src/app/routes/home.tsx
+++ src/app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "JU Can Eat" },
+    { name: "description", content: "Welcome to ObiadUJ/JU Can Eat!" },
   ];
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  assetsInclude: ["**/*.ico"],
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
