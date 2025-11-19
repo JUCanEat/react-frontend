ff4c55af9b91d2e6cfaabd2a749d6dd04da7f041
kingazm <kinga.zmuda@icloud.com>
2025-11-19 18:10:43 +0100

    feat: Add bottom navigation bar component

--- app/components/overview/bottom_nav.tsx
+++ app/components/overview/bottom_nav.tsx
@@ -1,1 +1,30 @@
-// contains Home, Map and Profile icons
+import { ItemActions } from "~/shadcn/components/ui/item"
+import { Button } from "~/shadcn/components/ui/button"
+import { Home, Map, User } from "lucide-react"
+
+export function BottomNav() {
+    return (
+        <div className="flex w-full justify-center">
+            <nav className="flex items-center gap-8 py-3">
+                <Button
+                    variant="ghost"
+                    onClick={() => alert("navigation to /")}
+                    >
+                    <Home className=""/>
+                </Button>
+                <Button
+                    variant="ghost"
+                    onClick={() => alert("navigation to /map")}
+                    >
+                    <Map className=""/>
+                </Button>
+                <Button
+                    variant="ghost"
+                    onClick={() => alert("navigation to /profile")}
+                    >
+                    <User className=""/>
+                </Button>
+            </nav>
+        </div>
+    )
+}
