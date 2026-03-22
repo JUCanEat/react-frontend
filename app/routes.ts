2124deca6cec2d7138ae730bfd94ee20bbe64cca
Martyna Cios <ciosmartyna@gmail.com>
2026-03-22 18:33:40 +0100

    small correction

--- app/routes.ts
+++ app/routes.ts
@@ -6,12 +6,12 @@ export default [
         route("restaurants/:restaurantId","routes/restaurant.tsx"),
         route("staff/menu-from-photo/:restaurantId", "routes/staff_menu_from_photo.tsx"),
         route("staff/menu-draft/:restaurantId", "routes/staff_menu_draft.tsx"),
+        route("*", "routes/not-found.tsx"),
     ]),
     route("login","routes/login.tsx"),
     route("profile","routes/profile.tsx"),
     route("map","routes/map.tsx"),
     route("menu", "routes/menu.tsx"),
     route("dishes","routes/dishes.tsx"),
-    route("staff/menu-from-form/:restaurantId", "routes/menu_form.tsx"),
-    route("*", "routes/not-found.tsx")
+    route("staff/menu-from-form/:restaurantId", "routes/menu_form.tsx")
 ] satisfies RouteConfig;
