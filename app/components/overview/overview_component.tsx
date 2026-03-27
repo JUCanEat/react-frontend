d9523606a71bd80bb14654f8734a5e8b191d22d8
Kinga Żmuda <kinga.zmuda@icloud.com>
2026-03-27 19:02:52 +0100

    fix: topbar height on mobile

--- app/components/overview/overview_component.tsx
+++ app/components/overview/overview_component.tsx
@@ -19,9 +19,9 @@ export function OverviewComponent() {
   const [filters, setFilters] = React.useState<FilterValue[]>([]);
   const { t } = useTranslation();
   return (
-    <div className={'flex justify-center dark:bg-zinc-950 min-h-screen overflow-hidden'}>
-      <div className={'dark:bg-zinc-950 max-w-3xl w-full flex flex-col'}>
-        <TopBar isLoginPage={false} />
+    <div className={'w-full dark:bg-zinc-950 min-h-screen overflow-hidden flex flex-col'}>
+      <TopBar isLoginPage={false} />
+      <div className={'dark:bg-zinc-950 max-w-3xl w-full mx-auto flex-1 flex flex-col'}>
         <div className={'pt-5 pl-5 pr-5 dark:bg-zinc-950 flex-1 flex flex-col'}>
           <div className="flex-1">
             <div className={'pt-5'}>
